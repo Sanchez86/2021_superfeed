3e365e9c345f2fa9c639ce19e808e5c8e2ad9e0f
Aleksandr Koniev <0719118@gmail.com>
2024-01-23 20:13:46 +0200

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    import StepsWindow from './components/StepsWindow.svelte';
+</script>
+
+<style lang="scss" global>
+	@import "sass/_variables.scss";
+	@import "sass/_mixin.scss";
+    @import "sass/_general.scss";
+</style>
+
+<div id="wrapper">
+
+    <StepsWindow />
+
+</div>
